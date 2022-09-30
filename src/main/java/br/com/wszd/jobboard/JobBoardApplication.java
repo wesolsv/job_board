5de402ada4f68f58dc17f623d15f779682fd8b22
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-09-29 21:36:23 -0300

    config basica spring boot

---
+++ src/main/java/br/com/wszd/jobboard/JobBoardApplication.java
@@ -0,0 +1,13 @@
+package br.com.wszd.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardApplication.class, args);
+	}
+
+}
