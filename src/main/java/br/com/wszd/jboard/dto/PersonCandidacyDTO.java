35e868df29387a8caadbfbb3b7ce0196dad18316
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-10-04 21:41:00 -0300

    inclusao do builder no dto

--- src/main/java/br/com/wszd/jboard/dto/PersonCandidacyDTO.java
+++ src/main/java/br/com/wszd/jboard/dto/PersonCandidacyDTO.java
@@ -1,9 +1,6 @@
 package br.com.wszd.jboard.dto;
 
-import br.com.wszd.jboard.model.Company;
-import br.com.wszd.jboard.model.Job;
-import br.com.wszd.jboard.model.JobPublished;
-import br.com.wszd.jboard.model.Person;
+import br.com.wszd.jboard.model.*;
 import br.com.wszd.jboard.util.CandidacyStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
@@ -13,7 +10,9 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -32,18 +31,66 @@ public class PersonCandidacyDTO {
     private JobPublished jobPublished;
 
     @JsonIgnoreProperties(ignoreUnknown = true, value = {"id","personId","companyId"})
-    private Job job;
+    private Optional<Job> job;
 
     @JsonIgnoreProperties(ignoreUnknown = true, value =  {"id", "phone", "cnpj", "jobs"})
-    private Company company;
+    private Optional<Company> company;
 
+
+    public static class Builder{
+        private LocalDateTime dataCandidacy;
+        private CandidacyStatus status;
+        private Person person;
+        private JobPublished jobPublished;
+        private Optional<Job> job;
+        private Optional<Company> company;
+
+        public PersonCandidacyDTO.Builder dataCandidacy(LocalDateTime dataCandidacy){
+            this.dataCandidacy = dataCandidacy;
+            return this;
+        }
+        public PersonCandidacyDTO.Builder status(CandidacyStatus status){
+            this.status = status;
+            return this;
+        }
+        public PersonCandidacyDTO.Builder person(Person person){
+            this.person = person;
+            return this;
+        }
+        public PersonCandidacyDTO.Builder job(Optional<Job> job){
+            this.job = job;
+            return this;
+        }
+        public PersonCandidacyDTO.Builder company(Optional<Company> company){
+            this.company = company;
+            return this;
+        }
+        public PersonCandidacyDTO.Builder jobPublished(JobPublished jobPublished){
+            this.jobPublished = jobPublished;
+            return this;
+        }
+
+        public PersonCandidacyDTO build(){
+            return new PersonCandidacyDTO(this);
+        }
+
+    }
+
+    private PersonCandidacyDTO(PersonCandidacyDTO.Builder builder){
+        this.dataCandidacy = dataCandidacy;
+        this.status = status;
+        this.person = person;
+        this.jobPublished = jobPublished;
+        this.job = Optional.ofNullable(jobPublished.getJobId());
+        this.company = Optional.ofNullable(job.get().getCompanyId());
+    }
 
     public PersonCandidacyDTO(LocalDateTime dataCandidacy, CandidacyStatus status, Person person, JobPublished jobPublished) {
         this.dataCandidacy = dataCandidacy;
         this.status = status;
         this.person = person;
         this.jobPublished = jobPublished;
-        this.job = jobPublished.getJobId();
-        this.company = job.getCompanyId();
+        this.job = Optional.ofNullable(jobPublished.getJobId());
+        this.company = Optional.ofNullable(job.get().getCompanyId());
     }
 }
