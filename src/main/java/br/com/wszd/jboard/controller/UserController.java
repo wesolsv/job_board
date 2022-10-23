c7b4238b9742428fff31b4b9bc6cf3d7aec75fda
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-10-23 14:01:24 -0300

    configurando swagger para utilizar autenticaçcao e JWT

--- src/main/java/br/com/wszd/jboard/controller/UserController.java
+++ src/main/java/br/com/wszd/jboard/controller/UserController.java
@@ -2,18 +2,11 @@ package br.com.wszd.jboard.controller;
 
 import br.com.wszd.jboard.dto.SessaoDTO;
 import br.com.wszd.jboard.dto.UserLoginDTO;
-import br.com.wszd.jboard.dto.UserRoleDTO;
-import br.com.wszd.jboard.exceptions.ResourceBadRequestException;
 import br.com.wszd.jboard.model.Users;
-import br.com.wszd.jboard.security.JWTCreator;
-import br.com.wszd.jboard.security.JWTObject;
-import br.com.wszd.jboard.security.SecurityConfig;
 import br.com.wszd.jboard.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
