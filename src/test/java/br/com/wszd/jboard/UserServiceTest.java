60379b10cfb4fc981cb8fd15048b2a7db539f258
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-10-29 12:08:14 -0300

    testes de user service implementados

--- src/test/java/br/com/wszd/jboard/UserServiceTest.java
+++ src/test/java/br/com/wszd/jboard/UserServiceTest.java
@@ -1,6 +1,9 @@
 package br.com.wszd.jboard;
 
+import br.com.wszd.jboard.dto.SessaoDTO;
+import br.com.wszd.jboard.dto.UserLoginDTO;
 import br.com.wszd.jboard.model.Person;
+import br.com.wszd.jboard.model.Role;
 import br.com.wszd.jboard.model.Users;
 import br.com.wszd.jboard.repository.UserRepository;
 import br.com.wszd.jboard.service.UserService;
@@ -9,12 +12,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-public class UserControllerTest{
+public class UserServiceTest {
 
     @Mock
     private UserRepository repository;
@@ -22,18 +28,12 @@ public class UserControllerTest{
     @InjectMocks
     private UserService service;
 
-//    @BeforeEach
-//    public void setUp() throws Exception {
-//        createPerson();
-//    }
     @Test
     public void shouldCreateUser() throws Exception {
-        Person person = new Person();
-
         Users user = new Users.Builder()
                 .email("wes@test2e.com.br")
                 .password("123456")
-                .personId(person)
+                .personId(new Person())
                 .companyId(null)
                 .build();
 
@@ -42,5 +42,28 @@ public class UserControllerTest{
 
         assertNotNull(user);
         assertEquals("wes@test2e.com.br", user.getEmail());
+    }
+
+    @Test
+    public void shouldLogin() throws Exception {
+        List<Role> roles = Arrays.asList(new Role(1L, "ADMIN"));
+
+        Users user = new Users.Builder()
+                .email("wes@test2e.com.br")
+                .password("$2a$08$sOOxkOE/arGYc6N1IBdzxO8kaWB7HWqlg/mhANhGeazRdDALX9vWK")
+                .personId(new Person())
+                .companyId(null)
+                .build();
+
+        user.setRoles(roles);
+
+        UserLoginDTO login = new UserLoginDTO("wes@test2e.com.br","123456");
+
+        SessaoDTO session = new SessaoDTO();
+
+        session = service.validLogin(user, login);
+
+        assertNotNull(session);
+        assertEquals("wes@test2e.com.br", session.getLogin());
     }
 }
